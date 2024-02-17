91714949f0596910a27d000d49e13cf81d68d200
s0nhaaa <nguyensonha.hanz@gmail.com>
2024-02-17 22:13:06 +0700

    feat: init mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+import { StatusBar } from 'expo-status-bar'
+
+export default function App() {
+  return (
+    <View className=" flex h-screen w-screen items-center justify-center bg-green-300">
+      <Text>Open up App.tsx to start working on yoasdur app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
