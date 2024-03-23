f466087475545d7c2aa56cb2092daf1257062780
9thdat <nthdat09@gmail.com>
2024-03-23 17:51:30 +0700

    style: update gallery ui

--- www/app/showcase/components/gallery.tsx
+++ www/app/showcase/components/gallery.tsx
@@ -3,7 +3,7 @@ import showcase from "@/public/showcase.svg";
 import sunflower from "@/public/sunflower.svg";
 import sticker_club from "@/public/sticker_club.svg";
 import test_img from "@/public/test_img.svg";
-
+import {Knewave} from "next/dist/compiled/@next/font/dist/google";
 const Gallery = () => {
   return (
     <main>
@@ -21,39 +21,54 @@ const Gallery = () => {
         <img src={sunflower.src} alt="sunflower" className="w-[10%] h-[10%]" />
       </div>
 
-      <div className="px-[15%] flex flex-col place-items-center">
+      <div className="">
         {[...Array(3)].map((_, i) => (
-          <div className={"my-[3%]"} key={i}>
-            <div>
-              <div className="border-2 border-spacing- border-dashed w-60 h-96 inline-block border-[#C47D7D] rounded-lg">
-                <img
-                  src={test_img.src}
-                  alt="test_img"
-                  className="w-[100%] h-[100%]"
-                />
-              </div>
-            </div>
+            <div className={"flex justify-center gap-[10%] mb-[5%]"} key={i}>
+                <div className={"border border-black relative pt-[2%]"}>
+                    <div className={"absolute inset-0 flex justify-center text-xl"}>Hello</div>
+                    <div className="border-2 border-dashed w-60 h-96 border-[#C47D7D] rounded-lg">
+                        <img
+                            src={test_img.src}
+                            alt="test_img"
+                            className="w-[100%] h-[100%]"
+                        />
+                    </div>
+                    <div className={"flex justify-between"}>
+                        <div className="text-xl">$30STKS/h</div>
+                        <div className="text-lg">ICON</div>
+                    </div>
+                </div>
 
-            <div>
-              <div className="border-2 border-spacing- border-dashed w-60 h-96 inline-block border-[#CCB4DF] rounded-lg mx-20">
-                <img
-                  src={test_img.src}
-                  alt="test_img"
-                  className="w-[100%] h-[100%]"
-                />
-              </div>
-            </div>
+                <div className={"border border-black relative pt-[2%]"}>
+                    <div className={"absolute inset-0 flex justify-center text-xl"}>Hello</div>
+                    <div className="border-2 border-dashed w-60 h-96 border-[#CCB4DF] rounded-lg">
+                        <img
+                            src={test_img.src}
+                            alt="test_img"
+                            className="w-[100%] h-[100%]"
+                        />
+                    </div>
+                    <div className={"flex justify-between"}>
+                        <div className="text-xl">$30STKS/h</div>
+                        <div className="text-lg">ICON</div>
+                    </div>
+                </div>
 
-            <div>
-              <div className="border-2 border-spacing- border-dashed w-60 h-96 inline-block border-[#8BBADC] rounded-lg">
-                <img
-                  src={test_img.src}
-                  alt="test_img"
-                  className="w-[100%] h-[100%]"
-                />
-              </div>
+                <div className={"border border-black relative pt-[2%]"}>
+                    <div className={"absolute inset-0 flex justify-center text-xl"}>Hello</div>
+                    <div className="border-2 border-dashed w-60 h-96 border-[#8BBADC] rounded-lg">
+                        <img
+                            src={test_img.src}
+                            alt="test_img"
+                            className="w-[100%] h-[100%]"
+                        />
+                    </div>
+                    <div className={"flex justify-between"}>
+                        <div className="text-xl">$30STKS/h</div>
+                        <div className="text-lg">ICON</div>
+                    </div>
+                </div>
             </div>
-          </div>
         ))}
       </div>
     </main>
